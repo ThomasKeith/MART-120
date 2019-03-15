be0f8bbcb02cef46fef3dc73247b76b985c8ffaa
ThomasKeith <46757607+ThomasKeith@users.noreply.github.com>
2019-03-15 17:18:37 -0600

    Added characters

--- Homework 9/hw9.js
+++ Homework 9/hw9.js
@@ -1,34 +1,68 @@
-var enemies
-var x
-var y
-var enemyX
-var enemyY
+let img, enemy, jessie, james;
+var x = 500
+var y = 100
+var enemyX = 500
+var enemyY = 600
+var jessieX = 300
+var jessieY = 600
+var jamesX = 700
+var jamesY = 600
+var canvasWidth = 1000
+var canvasHeight = 800
 
-
-
-
+function preload() {
+    img = loadImage('assets/pikachu.png');
+    enemy = loadImage('assets/meowth.png');
+    jessie = loadImage('assets/jessie.png');
+    james = loadImage('assets/james.png');
+}
 
 function setup() {
-    createCanvas(1000,800);
+    createCanvas(canvasWidth,canvasHeight);
 
 }
 
-function draw(){
+function draw() {
 
-    background(0,0,55);
+    background(0,20,45);
+    image(img,x,y,100,100);
+    playerController(7);
+    image(enemy,enemyX,enemyY,62,75);
+    enemyChase(1);
+    //rect(x,y,50,50);
+    
 
-    enemyChase(2);
+    //enemyChase(2);
 
-    //boundaries (are with the player controlls)
-    // in the controll function
-    // if(x > 0)...etc for all variables
-    // then this will allow the player to move in the appropriate boundaries
-
+}
+// controlls with boundaries
+function playerController(speed) {
+    if(keyIsPressed){
+        if(key =="a")
+        {
+            if(x > 0){
+                 x-=speed;
+            } 
+        }
+       else if(key == "d")
+        {
+           if(x < (canvasWidth - 50)){
+                 x+=speed;
+           } 
+        }
+       else if(key == "w")
+        {
+            if(y > 0){
+                y-=speed;
+            }
+        }
+       else if(key == "s")
+        {
+            if(y < (canvasHeight - 50)){
+                y+=speed;
+            }
+        }
     }
-}
-//edit
-function keyPressed(){
-
 }
 
 function enemyChase(speed){
@@ -45,8 +79,4 @@ function enemyChase(speed){
     if(enemyY < y){
         enemyY += speed;
     }
-}
-//edit
-function playerController(){
-    if(keyIsPressed)
 }
